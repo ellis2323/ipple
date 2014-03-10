f72f9cc48f393d7e9ccdd17148eb37d6e2846330
blckwd <corentin@blckwd.com>
2014-03-10 01:17:08 +0100

    bdd

--- bdd.sql
+++ bdd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Dim 09 Mars 2014 à 15:49
+-- Généré le : Lun 10 Mars 2014 à 00:16
 -- Version du serveur: 5.1.44
 -- Version de PHP: 5.3.1
 
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `addresses` (
   `city_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Contenu de la table `addresses`
@@ -49,7 +49,10 @@ INSERT INTO `addresses` (`id`, `firstname`, `lastname`, `phone`, `street`, `floo
 (2, 'Corentin', 'Chateil', '', '00 ', 5, 'zerzer', 'zerzer', 1, 1, 2),
 (3, 'Corentin', 'Chateil', '', '00 ', 5, 'qsdd', 'ssdfsdf', 1, 1, 2),
 (4, 'Corentin', 'Chateil', '', '00 ', 6, '<wx<wx<wx', '<<x<w', 1, 1, 4),
-(5, 'Clodo', 'Pedro', '', '1 rue des corbeaux', 5, 'Babedidouu', 'A1957', 1, 1, 0);
+(5, 'Clodo', 'Pedro', '', '1 rue des corbeaux', 5, 'Babedidouu', 'A1957', 1, 1, 0),
+(6, 'Corentin', 'Chateil', '', '00 ', 5, 'zeazeazeaez', 'ssdfsdf', 1, 1, 2),
+(7, 'Corentin', 'Chateil', '', '00 ', 4, 'qsdqsd', 'qsdqsd', 1, 1, 2),
+(8, 'Corentin', 'Chateil', '', '00 ', 5, 'qdqssd', 'ssdfsdf', 1, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -60,71 +63,100 @@ INSERT INTO `addresses` (`id`, `firstname`, `lastname`, `phone`, `street`, `floo
 CREATE TABLE IF NOT EXISTS `bacs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `media_id` int(11) NOT NULL,
-  `order_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `modified` datetime NOT NULL,
   `created` datetime NOT NULL,
-  `state` tinyint(1) NOT NULL,
+  `state` int(2) NOT NULL,
   `code` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=51 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=71 ;
 
 --
 -- Contenu de la table `bacs`
 --
 
-INSERT INTO `bacs` (`id`, `media_id`, `order_id`, `title`, `description`, `modified`, `created`, `state`, `code`) VALUES
-(1, 0, NULL, 'qsdqsd', '', '2014-03-09 02:56:21', '2014-03-09 02:40:50', 0, 'test1'),
-(2, 3, 4, 'Titre du bac', 'Description du bac', '2014-03-09 16:37:37', '2014-03-09 02:40:50', 0, 'test2'),
-(3, 0, 4, '', '', '2014-03-09 15:28:07', '2014-03-09 02:40:50', 0, 'test3'),
-(4, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test4'),
-(5, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test5'),
-(6, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test6'),
-(7, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test7'),
-(8, 0, 4, 'Test', 'qdqsdqsd', '2014-03-09 15:33:39', '2014-03-09 02:40:50', 0, 'test8'),
-(9, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test9'),
-(10, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test10'),
-(11, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test11'),
-(12, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test12'),
-(13, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test13'),
-(14, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test14'),
-(15, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test15'),
-(16, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test16'),
-(17, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test17'),
-(18, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test18'),
-(19, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test19'),
-(20, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test20'),
-(21, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test21'),
-(22, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test22'),
-(23, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test23'),
-(24, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test24'),
-(25, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test25'),
-(26, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test26'),
-(27, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test27'),
-(28, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test28'),
-(29, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test29'),
-(30, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test30'),
-(31, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test31'),
-(32, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test32'),
-(33, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test33'),
-(34, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test34'),
-(35, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test35'),
-(36, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test36'),
-(37, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test37'),
-(38, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test38'),
-(39, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test39'),
-(40, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test40'),
-(41, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test41'),
-(42, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test42'),
-(43, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test43'),
-(44, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test44'),
-(45, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test45'),
-(46, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test46'),
-(47, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test47'),
-(48, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test48'),
-(49, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test49'),
-(50, 0, NULL, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test50');
+INSERT INTO `bacs` (`id`, `media_id`, `user_id`, `title`, `description`, `modified`, `created`, `state`, `code`) VALUES
+(1, 4, 2, 'Titre du bac 1', 'Description du bac 2\r\n', '2014-03-10 01:14:52', '2014-03-09 02:40:50', 3, 'test1'),
+(2, 1, 2, 'Titre du bac', 'Description du bac', '2014-03-10 01:14:52', '2014-03-09 02:40:50', 3, 'test2'),
+(3, 0, 2, '', '', '2014-03-10 01:10:45', '2014-03-09 02:40:50', 0, 'test3'),
+(4, 0, 2, '', '', '2014-03-10 01:13:39', '2014-03-09 02:40:50', 0, 'test4'),
+(5, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test5'),
+(6, 0, 2, '', '', '2014-03-09 17:26:38', '2014-03-09 02:40:50', 0, 'test6'),
+(7, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test7'),
+(8, 0, 2, 'Test', 'qdqsdqsd', '2014-03-09 15:33:39', '2014-03-09 02:40:50', 0, 'test8'),
+(9, 0, 2, '', '', '2014-03-10 01:15:29', '2014-03-09 02:40:50', 3, 'test9'),
+(10, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test10'),
+(11, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test11'),
+(12, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test12'),
+(13, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test13'),
+(14, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test14'),
+(15, 0, 2, '', '', '2014-03-09 17:06:01', '2014-03-09 02:40:50', 0, 'test15'),
+(16, 0, 2, '', '', '2014-03-10 01:15:29', '2014-03-09 02:40:50', 3, 'test16'),
+(17, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test17'),
+(18, 0, 2, '', '', '2014-03-10 01:11:26', '2014-03-09 02:40:50', 0, 'test18'),
+(19, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test19'),
+(20, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test20'),
+(21, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test21'),
+(22, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test22'),
+(23, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test23'),
+(24, 0, 2, '', '', '2014-03-10 01:15:29', '2014-03-09 02:40:50', 3, 'test24'),
+(25, 0, 2, '', '', '2014-03-09 17:26:15', '2014-03-09 02:40:50', 0, 'test25'),
+(26, 0, 2, '', '', '2014-03-09 17:19:04', '2014-03-09 02:40:50', 0, 'test26'),
+(27, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test27'),
+(28, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test28'),
+(29, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test29'),
+(30, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test30'),
+(31, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test31'),
+(32, 0, 2, '', '', '2014-03-10 01:15:29', '2014-03-09 02:40:50', 3, 'test32'),
+(33, 0, 2, '', '', '2014-03-10 01:15:29', '2014-03-09 02:40:50', 3, 'test33'),
+(34, 0, 2, '', '', '2014-03-10 01:15:29', '2014-03-09 02:40:50', 3, 'test34'),
+(35, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test35'),
+(36, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test36'),
+(37, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test37'),
+(38, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test38'),
+(39, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test39'),
+(40, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test40'),
+(41, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test41'),
+(42, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test42'),
+(43, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test43'),
+(44, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test44'),
+(45, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test45'),
+(46, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test46'),
+(47, 0, 2, '', '', '2014-03-09 02:40:50', '2014-03-09 02:40:50', 0, 'test47'),
+(48, 0, 2, '', '', '2014-03-09 17:35:05', '2014-03-09 02:40:50', 0, 'test48'),
+(49, 0, 2, '', '', '2014-03-10 01:14:52', '2014-03-09 02:40:50', 3, 'test49'),
+(50, 0, 2, '', '', '2014-03-10 01:14:52', '2014-03-09 02:40:50', 3, 'test50');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `bacs_orders`
+--
+
+CREATE TABLE IF NOT EXISTS `bacs_orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bac_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+--
+-- Contenu de la table `bacs_orders`
+--
+
+INSERT INTO `bacs_orders` (`id`, `bac_id`, `order_id`) VALUES
+(1, 1, 1),
+(2, 2, 1),
+(3, 49, 1),
+(4, 50, 1),
+(5, 9, 2),
+(6, 16, 2),
+(7, 24, 2),
+(8, 32, 2),
+(9, 33, 2),
+(10, 34, 2);
 
 -- --------------------------------------------------------
 
@@ -146,32 +178,6 @@ CREATE TABLE IF NOT EXISTS `cities` (
 INSERT INTO `cities` (`id`, `label`, `state`) VALUES
 (1, 'Paris', 0),
 (2, 'Marseille', 0);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `deliveries`
---
-
-CREATE TABLE IF NOT EXISTS `deliveries` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `date` datetime DEFAULT NULL COMMENT 'Date de la livraison',
-  `state` tinyint(1) DEFAULT NULL COMMENT 'Type de livraison',
-  `delivery_id` int(11) DEFAULT NULL COMMENT 'Id de la livraison parente',
-  `address_id` int(11) DEFAULT NULL,
-  `hour_id` int(11) DEFAULT NULL,
-  `order_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COMMENT='Table contenant les livraisons' AUTO_INCREMENT=3 ;
-
---
--- Contenu de la table `deliveries`
---
-
-INSERT INTO `deliveries` (`id`, `date`, `state`, `delivery_id`, `address_id`, `hour_id`, `order_id`, `user_id`) VALUES
-(1, '2034-01-01 00:00:00', NULL, NULL, 0, 0, 4, 2),
-(2, '2014-03-09 00:00:00', NULL, 1, 0, NULL, 4, 2);
 
 -- --------------------------------------------------------
 
@@ -206,14 +212,15 @@ CREATE TABLE IF NOT EXISTS `locks` (
   `bac_id` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
-  `state` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
 -- Contenu de la table `locks`
 --
 
+INSERT INTO `locks` (`id`, `bac_id`, `created`, `modified`) VALUES
+(1, 15, '2014-03-09 17:00:06', '2014-03-09 17:00:51');
 
 -- --------------------------------------------------------
 
@@ -230,7 +237,7 @@ CREATE TABLE IF NOT EXISTS `medias` (
   PRIMARY KEY (`id`),
   KEY `ref` (`ref`),
   KEY `ref_id` (`ref_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Contenu de la table `medias`
@@ -239,7 +246,9 @@ CREATE TABLE IF NOT EXISTS `medias` (
 INSERT INTO `medias` (`id`, `ref`, `ref_id`, `file`, `position`) VALUES
 (1, 'Bac', 2, '/img/uploads/2014/03/8641.jpg', 2),
 (2, 'Bac', 2, '/img/uploads/2014/03/1380459_530044937073513_841769712_n.jpg', 3),
-(3, 'Bac', 2, '/img/uploads/2014/03/yeah_science_bitch_meme.jpg', 1);
+(3, 'Bac', 2, '/img/uploads/2014/03/yeah_science_bitch_meme.jpg', 1),
+(4, 'Bac', 1, '/img/uploads/2014/03/1545766_10201068079799364_541670278_n.jpg', 3),
+(5, 'Bac', 1, '/img/uploads/2014/03/yeah_science_bitch_meme-1.jpg', 2);
 
 -- --------------------------------------------------------
 
@@ -251,23 +260,26 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `address_id` int(11) NOT NULL,
-  `date_deposit` datetime NOT NULL,
-  `hour_deposit` int(11) NOT NULL,
+  `date_deposit` datetime DEFAULT NULL,
+  `hour_deposit` int(11) DEFAULT NULL,
+  `state_deposit` int(11) DEFAULT NULL,
   `date_withdrawal` datetime DEFAULT NULL,
   `hour_withdrawal` int(11) DEFAULT NULL,
-  `nb_bacs` int(11) NOT NULL COMMENT 'Nombre de bac commandé',
+  `state_withdrawal` int(11) DEFAULT NULL,
+  `nb_bacs` int(11) DEFAULT NULL COMMENT 'Nombre de bac commandé',
   `state` int(11) DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COMMENT='Permet de gérer les commandes et à lier les différentes livr' AUTO_INCREMENT=5 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COMMENT='Permet de gérer les commandes et à lier les différentes livr' AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `orders`
 --
 
-INSERT INTO `orders` (`id`, `user_id`, `address_id`, `date_deposit`, `hour_deposit`, `date_withdrawal`, `hour_withdrawal`, `nb_bacs`, `state`, `created`, `modified`) VALUES
-(4, 2, 5, '2034-01-01 00:00:00', 0, '2014-03-09 00:00:00', NULL, 4, 1, '2014-03-09 02:12:21', '2014-03-09 16:38:52');
+INSERT INTO `orders` (`id`, `user_id`, `address_id`, `date_deposit`, `hour_deposit`, `state_deposit`, `date_withdrawal`, `hour_withdrawal`, `state_withdrawal`, `nb_bacs`, `state`, `created`, `modified`) VALUES
+(1, 2, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 3, '2014-03-10 01:14:52', '2014-03-10 01:14:52'),
+(2, 2, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 3, '2014-03-10 01:15:29', '2014-03-10 01:15:29');
 
 -- --------------------------------------------------------
 
